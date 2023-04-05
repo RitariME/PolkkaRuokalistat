427206e168676f2f0dd9d388bab5ce92a88c56a5
RitariME <joona.v.pirinen@gmail.com>
2023-04-05 21:33:48 +0300

    just incase i forgot smth

--- app/src/main/java/com/ritarime/polkkaruokalistat/ui/theme/Theme.kt
+++ app/src/main/java/com/ritarime/polkkaruokalistat/ui/theme/Theme.kt
@@ -57,8 +57,8 @@ fun PolkkaRuokalistatTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !darkTheme
         }
     }
 
